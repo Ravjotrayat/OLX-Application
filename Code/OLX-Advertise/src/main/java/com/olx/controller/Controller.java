be361b1ca6d81b41a9dacfc553b6a9b4f68a9151
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2025-12-06 00:42:00 +0530

    OLX

--- Code/OLX-Advertise/src/main/java/com/olx/controller/Controller.java
+++ Code/OLX-Advertise/src/main/java/com/olx/controller/Controller.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,24 +30,26 @@ public class Controller {
 	@Qualifier("MySQl_DB")
 	AdvertiseService advertiseService;
 
-	@GetMapping("/ad")
+	@GetMapping(value = "/ad",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<AdvertiseDto>>  getAllAdvertise()
 	{
 		return new ResponseEntity<>(advertiseService.getAllAdvertise(), HttpStatus.OK);
 	}
 
-	@GetMapping("/ad/{id}")
+	@GetMapping(value = "/ad/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AdvertiseDto>  getAdvertiseById(@PathVariable("id") int id )
 	{
 		return new ResponseEntity<>(advertiseService.getAdvertiseById(id),HttpStatus.OK);
 	}
 
-	@PostMapping("/ad/create")
-	public ResponseEntity<AdvertiseDto> createAdvertises(AdvertiseDto advertiseDto)
+	@PostMapping(value = "/ad/create",consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<AdvertiseDto> createAdvertises(@RequestBody AdvertiseDto advertiseDto)
 	{
 		AdvertiseDto advertiseDto2=advertiseService.createAdvertise(advertiseDto);
 		return new ResponseEntity<AdvertiseDto>(advertiseDto2,HttpStatus.CREATED);
 	}
-
+	
+	
 
 }
