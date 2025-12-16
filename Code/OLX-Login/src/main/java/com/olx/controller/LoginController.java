f458163ed9d3a3a5c4b794b11cf2750efb4d4dd2
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2025-12-17 00:13:53 +0530

    OLX

--- Code/OLX-Login/src/main/java/com/olx/controller/LoginController.java
+++ Code/OLX-Login/src/main/java/com/olx/controller/LoginController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RequestMapping(value = "/user")
 @RestController
 public class LoginController {
 
-	
+	@Operation(description = "Authenticate yourself")
 	@PostMapping(value = "/authenticate",consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getAuthenticate()
@@ -21,6 +23,7 @@ public class LoginController {
 		return new ResponseEntity<String>("Authenticate the User",HttpStatus.OK);
 	}
 	
+	@Operation(description = "logout yourself")
 	@DeleteMapping(value = "/logout",consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getLogout()
@@ -28,6 +31,7 @@ public class LoginController {
 		return new ResponseEntity<String>("Logout Successfully",HttpStatus.OK);
 	}
 	
+	@Operation(description = "New User Login")
 	@PostMapping(value = "",consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> newUserLogin()
@@ -35,6 +39,7 @@ public class LoginController {
 		return new ResponseEntity<String>("User Login successfully",HttpStatus.CREATED);
 	}
 	
+	@Operation(description = "Get user details")
 	@GetMapping(value = "",consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getUserInformation()
@@ -42,6 +47,7 @@ public class LoginController {
 		return new ResponseEntity<String>("Get user details",HttpStatus.FOUND);
 	}
 	
+	@Operation(description = "Token/tokens getting validated")
 	@GetMapping(value = "/token/validate",consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> validateToken()
--- Code/OLX-MasterData/src/main/java/com/olx/controller/MasterDataController.java
+++ Code/OLX-MasterData/src/main/java/com/olx/controller/MasterDataController.java
@@ -1,10 +1,30 @@
 package com.olx.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping(value = "/advertise")
 @RestController
 public class MasterDataController {
+	
+	@GetMapping(value = "/category", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getAllCategory()
+	{
+		return new ResponseEntity<>("Fetching all the categories of advertisement",HttpStatus.ACCEPTED);
+	}
+	
+	@GetMapping(value = "/status",consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getAllCategoryStatus()
+	{
+		return new ResponseEntity<>("Fetching all the STATUS of categories of advertisement",
+											HttpStatus.ACCEPTED);
+		
+	}
 
 }
